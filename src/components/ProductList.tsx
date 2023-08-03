c85050e6e0d1a0a95953a452dc093a67178c8383
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-08-03 03:59:58 -0400

    feat: Add Spinner component fetching data

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,33 +1,30 @@
 import useCart from '../hooks/useCart';
 import useProducts from '../hooks/useProducts';
-import { ReactElement } from 'react';
 import Product from './Product';
+import Spinner from './Spinner';
 
-const ProductList = () => {
+export default function ProductList() {
   const { dispatch, REDUCER_ACTIONS, cart } = useCart();
   const { products } = useProducts();
 
-  let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>;
-
-  if (products?.length) {
-    pageContent = products.map((product) => {
-      const inCart: boolean = cart.some((item) => item.id === product.id);
-
-      return (
-        <Product
-          key={product.id}
-          product={product}
-          dispatch={dispatch}
-          REDUCER_ACTIONS={REDUCER_ACTIONS}
-          inCart={inCart}
-        />
-      );
-    });
-  }
+  if (products.length === 0) return <Spinner />;
 
   return (
-    <main className="flex flex-wrap gap-10 justify-between">{pageContent}</main>
-  );
-};
+    <main className="flex flex-wrap gap-10 justify-between">
+      {products?.length &&
+        products.map((product) => {
+          const inCart: boolean = cart.some((item) => item.id === product.id);
 
-export default ProductList;
+          return (
+            <Product
+              key={product.id}
+              product={product}
+              dispatch={dispatch}
+              REDUCER_ACTIONS={REDUCER_ACTIONS}
+              inCart={inCart}
+            />
+          );
+        })}
+    </main>
+  );
+}
